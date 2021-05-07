0bcf33c8427cf860bebd517c01e1f877ef0ce057
JinnyYi <jinnyyi@yunify.com>
2021-05-07 17:07:19 +0800

    modify delete error check

--- error.go
+++ error.go
@@ -6,11 +6,3 @@ var (
 	// ErrServerSideEncryptionCustomerKey will be returned while server-side encryption customer key is invalid.
 	ErrServerSideEncryptionCustomerKey = errors.New("invalid server-side encryption customer key")
 )
-
-// ErrCode
-//
-// ref: https://cloud.tencent.com/document/product/436/7730
-const (
-	// NoSuchKey the specified key does not exist.
-	NoSuchKey = "NoSuchKey"
-)
